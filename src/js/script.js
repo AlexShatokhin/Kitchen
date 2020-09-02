5c800b04874dddf9ad76ac13a1cedec2b9ec2bd3
Alexandr <shatokhin.04@inbox.ru>
2020-09-02 14:10:26 +0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,51 @@
+let callBtn = document.getElementById('recall'),
+	overlay = document.querySelector('.overlay'),
+	formClose = document.querySelector('.form_close'),
+	Viber = document.querySelector('.viber'),
+	WhatsApp = document.querySelector('.whatsapp'),
+	Telegram = document.querySelector('.telegram');
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+callBtn.addEventListener('click', function(){
+	overlay.style.display = 'block';
+});
+
+formClose.addEventListener('click', function(){
+	overlay.style.display = 'none';
+});
+
+Viber.addEventListener('click', function(){
+	Viber.classList.add('messenger_active');
+	WhatsApp.classList.remove('messenger_active');
+	Telegram.classList.remove('messenger_active');
+
+});
+
+WhatsApp.addEventListener('click', function(){
+	WhatsApp.classList.add('messenger_active');
+	Viber.classList.remove('messenger_active');
+	Telegram.classList.remove('messenger_active');
+
+
+});
+
+Telegram.addEventListener('click', function(){
+	Telegram.classList.add('messenger_active');
+	WhatsApp.classList.remove('messenger_active');
+	Viber.classList.remove('messenger_active');
+
+
+});
+
+for (let anchor of anchors) {
+	anchor.addEventListener('click', function (e) {
+	  e.preventDefault()
+	  
+	  const blockID = anchor.getAttribute('href').substr(1)
+	  
+	  document.getElementById(blockID).scrollIntoView({
+		behavior: 'smooth',
+		block: 'start'
+	  })
+	})
+  }
